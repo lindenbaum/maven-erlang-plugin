56b7f199f179c3d5dde5495db8749e0bca6fa77c
olle-t <olle-t@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2010-09-23 12:15:03 +0000

    Adapted integration tests and documentation to the goal name-change: coverage-report => coverage.

--- src/it/report/prebuild.groovy
+++ src/it/report/prebuild.groovy
@@ -1,4 +1,4 @@
-File target = new File(basedir, "target/site/erlang-coverage-report.html");
+File target = new File(basedir, "target/site/coverage/erlang-coverage-report.html");
 if (target.isFile()) {
     throw new IllegalStateException("The coverage report " + target + " must not exist before test is run.");
 }
