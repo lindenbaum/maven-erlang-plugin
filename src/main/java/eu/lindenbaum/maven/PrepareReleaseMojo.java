9646e70d94d0f5f1832317ab9552a0d9c89674b6
schlagert <schlagert@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2010-10-11 14:54:38 +0000

    fixed documentation

--- src/main/java/eu/lindenbaum/maven/PrepareReleaseMojo.java
+++ src/main/java/eu/lindenbaum/maven/PrepareReleaseMojo.java
@@ -60,7 +60,7 @@ import org.apache.maven.plugin.logging.Log;
  * </p>
  * 
  * <pre>
- * {release,"
+ * {release,
  *   {${ARTIFACT}, ${VERSION}},
  *   {erts, ${ERTS}},
  *   [{kernel, ${KERNEL}}, 
