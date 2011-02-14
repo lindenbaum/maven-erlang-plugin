4001a358c863b580817becfc11472acfa53a2ba0
schlagert <schlagert@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2011-02-14 16:17:02 +0000

    - added script to create an initial RELEASES file

---
+++ src/main/java/eu/lindenbaum/maven/erlang/CreateRELEASESScript.java
@@ -0,0 +1,43 @@
+package eu.lindenbaum.maven.erlang;
+
+import java.io.File;
+
+import com.ericsson.otp.erlang.OtpErlangObject;
+
+import eu.lindenbaum.maven.util.ErlUtils;
+
+/**
+ * A {@link Script} that creates an initial RELEASES file from a specific
+ * release file.
+ * 
+ * @author Tobias Schlager <tobias.schlager@lindenbaum.eu>
+ */
+public class CreateRELEASESScript implements Script<String> {
+  private static String script = NL + "release_handler:create_RELEASES(\"%s\", \"%s\", \"%s\", []).";
+
+  private final File rootDir;
+  private final File relFile;
+
+  public CreateRELEASESScript(File rootDir, File relFile) {
+    this.rootDir = rootDir;
+    this.relFile = relFile;
+  }
+
+  @Override
+  public String get() {
+    String rootDirectory = this.rootDir.getAbsolutePath();
+    String relDirectory = this.relFile.getParentFile().getAbsolutePath();
+    String relFile = this.relFile.getAbsolutePath();
+    return String.format(script, rootDirectory, relDirectory, relFile);
+  }
+
+  /**
+   * Converts the result of the {@link Script} execution into an error message.
+   * 
+   * @return "ok" in case of success, a failure message in case of errors
+   */
+  @Override
+  public String handle(OtpErlangObject result) {
+    return ErlUtils.toString(result);
+  }
+}
---
+++ src/test/java/eu/lindenbaum/maven/erlang/CreateRELEASESScriptTest.java
@@ -0,0 +1,48 @@
+package eu.lindenbaum.maven.erlang;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import com.ericsson.otp.erlang.OtpErlangAtom;
+import com.ericsson.otp.erlang.OtpErlangObject;
+import com.ericsson.otp.erlang.OtpErlangTuple;
+
+import org.junit.Test;
+
+public class CreateRELEASESScriptTest {
+  @Test
+  public void testGet() {
+    File rootDir = new File("/rootDir");
+    File relFile = new File("/rootDir/release.rel");
+    CreateRELEASESScript script = new CreateRELEASESScript(rootDir, relFile);
+    String expression = script.get();
+    assertNotNull(expression);
+    assertFalse(expression.isEmpty());
+    assertFalse(expression.contains("%s"));
+  }
+
+  @Test
+  public void testHandleOk() {
+    OtpErlangAtom result = new OtpErlangAtom("ok");
+
+    File rootDir = new File("/rootDir");
+    File relFile = new File("/rootDir/release.rel");
+    CreateRELEASESScript script = new CreateRELEASESScript(rootDir, relFile);
+    assertEquals("ok", script.handle(result));
+  }
+
+  @Test
+  public void testHandleError() {
+    OtpErlangAtom error = new OtpErlangAtom("error");
+    OtpErlangAtom reason = new OtpErlangAtom("reason");
+    OtpErlangTuple result = new OtpErlangTuple(new OtpErlangObject[]{ error, reason });
+
+    File rootDir = new File("/rootDir");
+    File relFile = new File("/rootDir/release.rel");
+    CreateRELEASESScript script = new CreateRELEASESScript(rootDir, relFile);
+    assertEquals("{error,reason}", script.handle(result));
+  }
+}
