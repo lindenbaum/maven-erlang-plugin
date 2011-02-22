d8f82abd1262939f1c069d89db73e4ee385b1134
olle-t <olle-t@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2011-02-22 18:20:09 +0000

    - added a few tests for ErlUtils string and integer conversion.

--- src/test/java/eu/lindenbaum/maven/util/ErlUtilsTest.java
+++ src/test/java/eu/lindenbaum/maven/util/ErlUtilsTest.java
@@ -10,6 +10,15 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.ericsson.otp.erlang.OtpErlangAtom;
+import com.ericsson.otp.erlang.OtpErlangInt;
+import com.ericsson.otp.erlang.OtpErlangList;
+import com.ericsson.otp.erlang.OtpErlangLong;
+import com.ericsson.otp.erlang.OtpErlangObject;
+import com.ericsson.otp.erlang.OtpErlangRangeException;
+import com.ericsson.otp.erlang.OtpErlangString;
+import com.ericsson.otp.erlang.OtpErlangUInt;
+
 import org.junit.Test;
 
 public class ErlUtilsTest {
@@ -50,5 +59,69 @@ public class ErlUtilsTest {
   public void testToModuleListNull() {
     String result = ErlUtils.toModuleList(new ArrayList<File>(), "1", "2");
     assertEquals("[]", result);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testToStringIsNotNullSafe() {
+    ErlUtils.toString(null);
+  }
+
+  @Test
+  public void testStringToString() {
+    assertEquals("foobar", ErlUtils.toString(new OtpErlangString("foobar")));
+  }
+
+  @Test
+  public void testAtomToString() {
+    assertEquals("foobar", ErlUtils.toString(new OtpErlangAtom("foobar")));
+  }
+
+  @Test
+  public void testListToString() {
+    OtpErlangInt one = new OtpErlangInt(1);
+    OtpErlangInt two = new OtpErlangInt(2);
+    OtpErlangInt three = new OtpErlangInt(3);
+    assertEquals("[1,2,3]", ErlUtils.toString(new OtpErlangList(new OtpErlangObject[]{ one, two, three })));
+  }
+
+  @Test
+  public void testEmptyListToString() {
+    assertEquals("", ErlUtils.toString(new OtpErlangList()));
+  }
+
+  @Test
+  public void ensureToIntIsNullSafe() {
+    assertEquals(0, ErlUtils.toInt(null));
+  }
+
+  @Test
+  public void ensureToIntDiscardsBadType() {
+    assertEquals(0, ErlUtils.toInt(new OtpErlangString("123")));
+    assertEquals(0, ErlUtils.toInt(new OtpErlangAtom("123")));
+  }
+
+  @Test
+  public void ensureToIntSilentlyEatsOverflowException() {
+    assertEquals(0, ErlUtils.toInt(new OtpErlangLong(Long.MAX_VALUE)));
+    assertEquals(0, ErlUtils.toInt(new OtpErlangLong(Long.MIN_VALUE)));
+  }
+
+  @Test
+  public void testToInt() throws OtpErlangRangeException {
+    assertEquals(0, ErlUtils.toInt(new OtpErlangLong(0)));
+    assertEquals(-123, ErlUtils.toInt(new OtpErlangLong(-123)));
+    assertEquals(123, ErlUtils.toInt(new OtpErlangLong(123)));
+    assertEquals(Integer.MIN_VALUE, ErlUtils.toInt(new OtpErlangLong(Integer.MIN_VALUE)));
+    assertEquals(Integer.MAX_VALUE, ErlUtils.toInt(new OtpErlangLong(Integer.MAX_VALUE)));
+
+    assertEquals(0, ErlUtils.toInt(new OtpErlangInt(0)));
+    assertEquals(-123, ErlUtils.toInt(new OtpErlangInt(-123)));
+    assertEquals(123, ErlUtils.toInt(new OtpErlangInt(123)));
+    assertEquals(Integer.MAX_VALUE, ErlUtils.toInt(new OtpErlangInt(Integer.MAX_VALUE)));
+    assertEquals(Integer.MIN_VALUE, ErlUtils.toInt(new OtpErlangInt(Integer.MIN_VALUE)));
+
+    assertEquals(0, ErlUtils.toInt(new OtpErlangUInt(0)));
+    assertEquals(123, ErlUtils.toInt(new OtpErlangUInt(123)));
+    assertEquals(Integer.MAX_VALUE, ErlUtils.toInt(new OtpErlangUInt(Integer.MAX_VALUE)));
   }
 }
