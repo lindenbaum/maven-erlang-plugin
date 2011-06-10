eb7a1bd654e6edacabaf53720abc28548f66acbb
olle-t <olle-t@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2011-06-10 09:06:44 +0000

    Modified profiling script to ensure that eunit is shutdown before the profiling runner starts and invokes the tests. Solves problems with profiling running after the testing phase.
    Also hardened the integration tests for profiling.

--- src/main/resources/eu/lindenbaum/maven/erlang/profilingscript.erl
+++ src/main/resources/eu/lindenbaum/maven/erlang/profilingscript.erl
@@ -2,29 +2,36 @@ ProfilingPath = "%s",
 ProfilingFile = "PROFILING-%s.txt",
 Tests = %s,
 Timeout = %s * 1000,
-Tty = {report, {ttycapture, [{report_to, self()}]}},
-Root = spawn(fun() ->
-		     receive
-			 {start, From} ->
-			     lists:map(fun(Test)->
-					       try eunit:test(Test, [Tty]) of
-						   error -> From ! {error, ["Test(s) failed!"]};
-						   _ -> ok
-					       catch
-						   Class:Exception ->
-						       From ! {error, [lists:flatten(io_lib:format("~p:~p", [Class, Exception]))]}
-					       end
-				       end, Tests)
-		     end
-	     end),
-eprof:start(),
-eprof:start_profiling([Root]),
-Root ! {start, self()},
+
+%% spawns a process that roots the eunit test invocations to its pid.
+%% all processes and funs that are run as children from this one will
+%% be profiled.
+Runner = spawn(fun() ->
+		       Tty = {report, {ttycapture, [{report_to, self()}]}},
+		       receive
+			   {start, Starter} ->
+			       eunit:stop(), %% note: eunit tests only profiled if spawned AFTER eprof is started.
+			       try eunit:test(Tests, [Tty]) of
+				   error -> Starter ! {error, nothing, ["Test(s) failed!"]};
+				   _ -> ok
+			       catch
+				   Cl:Ex ->
+				       Starter ! {error, nothing, [lists:flatten(io:format("~p:~p", [Cl, Ex]))]}
+			       end,
+			       receive
+				   TestResults -> Starter ! TestResults
+			       after Timeout ->
+				       Starter ! {error, nothing, ["Timeout, waiting for test results."]}
+			       end
+		       end
+	       end),
+{ok, _} = eprof:start(),
+profiling = eprof:start_profiling([Runner]),
+Runner ! {start, self()},
 Result = receive
-	     {Level, _Numbers, Messages} ->
-		 {Level, Messages}
+	     {Level, _, Messages} -> {Level, Messages}
 	 after Timeout ->
-	 	 {error, ["Timeout, waiting for results."]}
+	 	 {error, ["Timeout, waiting for profiling results."]}
 	 end,
 eprof:stop_profiling(),
 eprof:log(filename:join([ProfilingPath, ProfilingFile])),
