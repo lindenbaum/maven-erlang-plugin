a53487104a97d47c7e2d285fab45f801cb234f51
schlagert <schlagert@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2011-06-10 10:36:27 +0000

    - added mojo to check the project configuration for legal values - update eapp2mvn to latest version of Gregory Haskins' github repository

---
+++ src/main/java/eu/lindenbaum/maven/mojo/Validator.java
@@ -0,0 +1,35 @@
+package eu.lindenbaum.maven.mojo;
+
+import eu.lindenbaum.maven.ErlangMojo;
+import eu.lindenbaum.maven.Properties;
+
+import com.ericsson.otp.erlang.OtpErlangAtom;
+
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * {@link Mojo} that checks the project configuration for legal values.
+ * 
+ * @goal validate
+ * @phase validate
+ * @author Olle Törnström <olle.toernstroem@lindenbaum.eu>
+ * @author Tobias Schlager <tobias.schlager@lindenbaum.eu>
+ * @since 2.1.0
+ */
+public final class Validator extends ErlangMojo {
+  @Override
+  protected void execute(Log log, Properties p) throws MojoExecutionException, MojoFailureException {
+    MavenProject project = p.project();
+
+    String applicationName = new OtpErlangAtom(project.getArtifactId()).toString();
+    if (applicationName.startsWith("'")) {
+      log.error("The name " + applicationName + " is not a valid project artifact id.");
+      log.error("Artifact id must be an unquoted erlang atom, e.g. no dashes.");
+      throw new MojoExecutionException("Invalid artifact id " + applicationName + ".");
+    }
+  }
+}
