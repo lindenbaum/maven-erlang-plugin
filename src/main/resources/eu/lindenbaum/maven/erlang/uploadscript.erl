7de903f3a906eb14bc6d3edce747e791d5d6007b
olle-t <olle-t@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2011-07-21 11:14:12 +0000

    Changed resource upload script to better resolve temp-directories and add uploaded resources to the path of remote nodes.

--- src/main/resources/eu/lindenbaum/maven/erlang/uploadscript.erl
+++ src/main/resources/eu/lindenbaum/maven/erlang/uploadscript.erl
@@ -3,172 +3,181 @@ BeamFileList = %s,
 AppFileList = %s,
 ResourceList = %s,
 
-%%--------------------------------------------------------------------------
+%% --------------------------------------------------------------------------
 %% Utility Section
-%%--------------------------------------------------------------------------
+%% --------------------------------------------------------------------------
 
 DirPredicate =
 fun(Path) when is_list(Path) ->
-	rpc:call(Node, filelib, is_dir, [Path]);
+        rpc:call(Node, filelib, is_dir, [Path]);
    (_) ->
-	false
+        false
 end,
 
 GetEnvDirs = 
 fun() ->
-	Vars = ["$TMPDIR", "$TMP", "$TEMP"],
-	Paths = [rpc:call(Node, os, getenv, [Var]) || Var <- Vars],
-	lists:filter(DirPredicate, Paths)
+        Vars = ["TMPDIR", "TMP", "TEMP"],
+        Paths = [rpc:call(Node, os, getenv, [Var]) || Var <- Vars],
+        lists:filter(DirPredicate, Paths)
 end,
 
 GetDefaultDirs =
 fun() ->
-	Paths = ["/tmp", "/var/tmp"],
-	lists:filter(DirPredicate, Paths)
+        Paths = ["/tmp", "/var/tmp"],
+        lists:filter(DirPredicate, Paths)
 end,
 
 GetRemoteCwd =
 fun() ->
-	case rpc:call(Node, file, get_cwd, []) of
-	    {ok, Cwd} ->
-		[Cwd];
-	    _ ->
-		[]
-	end
+        case rpc:call(Node, file, get_cwd, []) of
+            {ok, Cwd} ->
+                [Cwd];
+            _ ->
+                []
+        end
 end,
 
+%% Returns the best possible temporary directory, as resolved by checking
+%% the 1) environment settings, 2) commonly used paths or 3) current
+%% working directory of the remote node; or the atom 'error' if no
+%% temp directory could be found.
 GetTmpDir =
 fun() ->
-	case GetEnvDirs() ++ GetDefaultDirs() ++ GetRemoteCwd() of
-	    [Tmp | _] ->
-		Tmp;
-	    _ ->
-		error
-	end
+        case GetEnvDirs() ++ GetDefaultDirs() ++ GetRemoteCwd() of
+            [Tmp | _] ->
+                Tmp;
+            _ ->
+                error
+        end
 end,
 
+%% Parses a given resource (file) and temp directory into a tuple containing
+%% the target root path per dependency and the relative path with the target
+%% resource.
+%%
+%% Example:
+%%      $PROJ/target/lib/dep1/priv/resource
+%%   yields:
+%%      {$TMP/dep1/, priv/resource}
+%%
 GetTargetResourcePath =
 fun(Resource, TmpDir) ->
-	Regex = "target/(lib/)?(.+/priv/.+)",
-	Opts = [{capture, all_but_first}],
-	{match, [_ | [{S, L} | _]]} = re:run(Resource, Regex, Opts),
-	Relative = string:substr(Resource, S + 1, L),
-	rpc:call(Node, filename, join, [[TmpDir, Relative]])
-end,
-
-GetTargetResourceRoot =
-fun(Resource, TmpDir) ->
-	Regex = "target/(.+)/",
-	Opts = [{capture, all_but_first}, ungreedy],
-	{match, [{S, L} | _]} = re:run(Resource, Regex, Opts),
-	Root = string:substr(Resource, S + 1, L),
-	rpc:call(Node, filename, join, [[TmpDir, Root]])
+        File = filename:basename(Resource),
+        Path = filename:dirname(Resource),
+        Es = lists:reverse(filename:split(Path)),
+        {PrivPath, Dependency} = 
+            lists:foldl(fun(E, {PP = ["priv" | _], []}) ->
+                                {PP, [E]};
+                           (E, {PP, []}) ->
+                                {[E | PP], []};                           
+                           (_, {PP, Dep}) ->
+                                {PP, Dep}
+                        end, {[], []}, Es),
+        TargetPath = rpc:call(Node, filename, join, [[TmpDir, Dependency]]),
+        RelativeResource = rpc:call(Node, filename, join, [[PrivPath, File]]),
+        {TargetPath, RelativeResource}
 end,
 
 UploadModule =
 fun(BeamFile) ->
-	Module = filename:basename(BeamFile, ".beam"),
-	case file:read_file(BeamFile) of
-	    {ok, Binary} ->
-		ModuleName = list_to_atom(Module),
-		LoadArgs = [ModuleName, BeamFile, Binary],
-		rpc:call(Node, code, purge, [ModuleName]),
-		rpc:call(Node, code, load_binary, LoadArgs),
-		BeamFile;
-
-	    Error ->
-		{BeamFile, Error}
-	end
+        Module = filename:basename(BeamFile, ".beam"),
+        case file:read_file(BeamFile) of
+            {ok, Binary} ->
+                ModuleName = list_to_atom(Module),
+                LoadArgs = [ModuleName, BeamFile, Binary],
+                rpc:call(Node, code, purge, [ModuleName]),
+                rpc:call(Node, code, load_binary, LoadArgs),
+                BeamFile;
+            Error ->
+                {BeamFile, Error}
+        end
 end,
 
 LoadApp =
 fun(AppFile) ->
 	case file:consult(AppFile) of
 	    {ok, [AppSpec]} ->
-		rpc:call(Node, application, load, [AppSpec]),
-		AppFile;
-
+          rpc:call(Node, application, load, [AppSpec]),
+          AppFile;
 	    Error ->
-		{AppFile, Error}
+          {AppFile, Error}
 	end
 end,
-
+ 
 UploadResource =
 fun(Resource, TmpDir) when is_list(TmpDir) ->
-	case file:read_file(Resource) of
-	    {ok, Binary} ->
-		Target = GetTargetResourcePath(Resource, TmpDir),
-		case rpc:call(Node, filelib, ensure_dir, [Target]) of
-		    ok ->
-			WriteArgs = [Target, Binary],
-			R = rpc:call(Node, file, write_file, WriteArgs),
-			case R of
-			    ok ->
-				ModeArgs = [Target, 755],
-				rpc:call(Node, file, change_mode, ModeArgs),
-				Resource;
-
-			    Error ->
-				{Resource, Error}
-			end;
-
-		    Error ->
-			{Resource, Error}
-		end;
-
-	    Error ->
-		{Resource, Error}
-	end;
-
+        case file:read_file(Resource) of
+            {ok, Binary} ->
+                {TargetPath, RelativeFile} = GetTargetResourcePath(Resource, TmpDir),
+                Target = rpc:call(Node, filename, join, [[TargetPath, RelativeFile]]),
+                case rpc:call(Node, filelib, ensure_dir, [Target]) of
+                    ok ->
+                        WriteArgs = [Target, Binary],
+                        R = rpc:call(Node, file, write_file, WriteArgs),
+                        case R of
+                            ok ->
+                                ModeArgs = [Target, 755],
+                                rpc:call(Node, file, change_mode, ModeArgs),
+                                Resource;
+                            Error ->
+                                {Resource, Error}
+                        end;
+                    Error ->
+                        {Resource, Error}
+                end;
+            Error ->
+                {Resource, Error}
+        end;
    (Resource, _) ->
-	{Resource, {skipped, no_tmp_directory_available}}
+        {Resource, {skipped, no_tmp_directory_available}}
 end,
 
 AddResourceToCodePath =
 fun(Resource, TmpDir) ->
-	Root = GetTargetResourceRoot(Resource, TmpDir),
-	Result = rpc:call(Node, code, add_patha, [Root])
+        {TargetPath, RelativeFile} = GetTargetResourcePath(Resource, TmpDir),
+        case rpc:call(Node, code, add_patha, [TargetPath]) of
+            true ->
+                ok;
+            {error, bad_directory} ->
+                {error, bad_directory, TargetPath}
+        end
 end,
 
-%%--------------------------------------------------------------------------
+%% --------------------------------------------------------------------------
 %% Script Section
-%%--------------------------------------------------------------------------
+%% --------------------------------------------------------------------------
 
 Tmp = GetTmpDir(),
 case net_kernel:connect(Node) of
     true ->
-	A = lists:foldl(
-	      fun(Beam, {Succeeded, Failed}) ->
-		      case UploadModule(Beam) of
-			  Beam ->
-			      {[Beam | Succeeded], Failed};
-
-			  Error ->
-			      {Succeeded, [Error | Failed]}
-		      end
-	      end, {[], []}, BeamFileList),
-	B = lists:foldl(
-	      fun(AppFile, {Succeeded, Failed}) ->
-		      case LoadApp(AppFile) of
-			  AppFile ->
-			      {[AppFile | Succeeded], Failed};
-
-			  Error ->
-			      {Succeeded, [Error | Failed]}
-		      end
-	      end, A, AppFileList),
-	lists:foldl(
-	  fun(Resource, {Succeeded, Failed}) ->
-		  case UploadResource(Resource, Tmp) of
-		      Resource ->
-			  AddResourceToCodePath(Resource, Tmp),
-			  {[Resource | Succeeded], Failed};
-
-		      Error ->
-			  {Succeeded, [Error | Failed]}
-		  end
-	  end, B, ResourceList);
-
+        A = lists:foldl(
+              fun(Beam, {Succeeded, Failed}) ->
+                      case UploadModule(Beam) of
+                          Beam ->
+                              {[Beam | Succeeded], Failed};
+                          Error ->
+                              {Succeeded, [Error | Failed]}
+                      end
+              end, {[], []}, BeamFileList),
+        B = lists:foldl(
+              fun(AppFile, {Succeeded, Failed}) ->
+                      case LoadApp(AppFile) of
+                          AppFile ->
+                              {[AppFile | Succeeded], Failed};
+                          Error ->
+                              {Succeeded, [Error | Failed]}
+                      end
+              end, A, AppFileList),
+        lists:foldl(
+          fun(Resource, {Succeeded, Failed}) ->
+                  case UploadResource(Resource, Tmp) of
+                      Resource ->
+                          ok = AddResourceToCodePath(Resource, Tmp),
+                          {[Resource | Succeeded], Failed};
+                      Error ->
+                          {Succeeded, [Error | Failed]}
+                  end
+          end, B, ResourceList);
     false ->
-	{error, {failed_to_connect_to, Node}}
+        {error, {failed_to_connect_to, Node}}
 end.
